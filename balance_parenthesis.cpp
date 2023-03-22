c996075c984561bc6e4d90da899efdacd9690ca4
sushant2106 <90310743+sushant2106@users.noreply.github.com>
2023-03-22 21:25:26 +0530

    Create balance_parenthesis.cpp
    to check whether String is balanced or not

---
+++ balance_parenthesis.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool balance(string s){
+    stack<char>st;
+      for(auto x:s)
+        {
+            if(x=='(' || x=='{' || x=='[')
+            {
+                st.push(x);
+            }
+            else
+            {
+              if(!st.empty())
+              {
+                    char m=st.top();
+                if(x=='}' && m=='{' || x==')' && m=='(' || x==']' && m=='[')
+                {
+                    st.pop();
+                }
+                else
+                   continue;
+              }
+              else
+                 return false;
+            }
+        }
+        return st.empty();
+}
+int main()
+{
+    string s = "(]";
+    cout<<balance(s);
+    
+
+}
